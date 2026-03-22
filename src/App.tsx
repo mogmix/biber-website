48786b890d72bf299abd8594fd65474fbeea5b4d
mogmix <dev@mogmix.org>
2026-03-22 21:37:07 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white dark:bg-gray-900">
+      <h1 className="text-4xl font-medium text-gray-900 dark:text-gray-100">
+        Hallo Welt
+      </h1>
+    </main>
+  )
+}
